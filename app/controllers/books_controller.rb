e30c0aa86e4171fdcc1a8af328249410c71ed96e
miyanew <dev38_shin@yahoo.co.jp>
2023-09-06 17:54:28 +0900

    Update keys in dictionary files and modify the conversion process

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,7 +27,7 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to book_url(@book), notice: 'Book was successfully created.' }
+        format.html { redirect_to book_url(@book), notice: I18n.t('flash.book_was_successfully_created') }
         format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -40,7 +40,7 @@ class BooksController < ApplicationController
   def update
     respond_to do |format|
       if @book.update(book_params)
-        format.html { redirect_to book_url(@book), notice: 'Book was successfully updated.' }
+        format.html { redirect_to book_url(@book), notice: I18n.t('flash.book_was_successfully_updated') }
         format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -54,7 +54,7 @@ class BooksController < ApplicationController
     @book.destroy
 
     respond_to do |format|
-      format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
+      format.html { redirect_to books_url, notice: I18n.t('flash.book_was_successfully_destroyed') }
       format.json { head :no_content }
     end
   end
