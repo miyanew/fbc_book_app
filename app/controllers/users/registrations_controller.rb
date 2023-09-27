01787aa12c4f8f11e81660a04af7d46923d76428
miyanew <dev38_shin@yahoo.co.jp>
2023-09-27 17:45:46 +0900

    Add functions to view and edit user profile.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,11 +3,17 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  before_action :authenticate_user!
 
   # GET /resource/sign_up
   # def new
   #   super
   # end
+
+  # GET /resource
+  def show
+    @user = User.find(current_user.id)
+  end
 
   # POST /resource
   # def create
@@ -38,7 +44,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
@@ -51,9 +57,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    super(resource)
+    users_path(id: current_user.id)
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_scope :user do
+    unauthenticated do
+      root to: 'users/sessions#new', as: :unauthenticated_root
+    end
+
+    root to: 'users/registrations#show'
+    get 'users', to: 'users/registrations#show'
+    get 'users/sign_out', to: 'users/sessions#destroy'
+  end
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   resources :books
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
