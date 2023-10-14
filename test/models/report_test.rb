f8897c55f4eaf76deeca47e1cb4cdb562a20be1b
miyanew <dev38_shin@yahoo.co.jp>
2023-10-14 23:25:10 +0900

    Add model test for Report class

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,17 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable?' do
+    user = User.create(email: 'foo@example.com')
+    report = Report.create(user: user)
+    assert report.editable?(user)
+
+    user2 = User.create(email: 'goo@example.com')
+    assert_not report.editable?(user2)
+  end
+
+  test '#created_on' do
+    report = Report.create(created_at: Time.new(2023, 10, 14, 12, 0, 0))
+    assert_equal Date.new(2023, 10, 14), report.created_on
+  end
 end
