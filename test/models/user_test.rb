f704139b378866684e9e53f08cac5022cf4b6d4e
miyanew <dev38_shin@yahoo.co.jp>
2023-11-07 02:41:15 +0900

    Update bad english sentences

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test 'should return email if name is blank, or name if name is present' do
+  test 'should return email if name is blank, or return name if name is present' do
     user = User.new(email: 'foo@example.com', name: '')
     assert_equal 'foo@example.com', user.name_or_email
 
