e4ea68c957c37d89f5004204957c728c0f2af036
miyanew <dev38_shin@yahoo.co.jp>
2023-11-19 22:25:13 +0900

    Added validation by file type

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
+  validate :avatar_type
 
   has_one_attached :avatar do |attachable|
     attachable.variant :thumb, resize_to_limit: [128, 128]
+  end
+
+  def avatar_type
+    return unless avatar.attached?
+    return if avatar.content_type.in?(%('image/jpeg image/png image/gif'))
+
+    errors.add(:avatar, :invalid_type)
   end
 end
