1a84601a034349ba0c2c0e31668cd9be0dd6238b
miyanew <dev38_shin@yahoo.co.jp>
2023-11-19 21:42:42 +0900

    Added the ability to resize icon images

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
+
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [128, 128]
+  end
 end
