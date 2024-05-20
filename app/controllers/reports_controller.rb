d7b226a331cb00ba024e7207836677954271815b
miyanew <dev38_shin@yahoo.co.jp>
2024-05-20 09:53:33 +0900

    report詳細を編集できる

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,9 +1,33 @@
 class ReportsController < ApplicationController
+  before_action :set_report, only: %i[show edit update destroy]
+
   def index
     @reports = Report.order(:id).page(params[:page])
   end
 
-  def show
+  def show;end
+
+  def edit;end
+
+  def update
+    respond_to do |format|
+      if @report.update(report_params)
+        format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human) }
+        format.json { render :show, status: :ok, location: @report }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @report.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_report
     @report = Report.find(params[:id])
+  end
+
+  def report_params
+    params.require(:report).permit(:title, :body)
   end
 end
