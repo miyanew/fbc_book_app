3be8a54f99166138c7b9840bb563132daaca8d81
miyanew <dev38_shin@yahoo.co.jp>
2024-06-27 03:07:10 +0900

    commentにも外部キー制約を追加した

--- db/migrate/20240609105859_add_user_id_to_comments.rb
+++ db/migrate/20240609105859_add_user_id_to_comments.rb
@@ -1,5 +1,7 @@
 class AddUserIdToComments < ActiveRecord::Migration[7.0]
   def change
     add_column :comments, :user_id, :integer
+    add_index :comments, :user_id
+    add_foreign_key :comments, :users
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -56,6 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_09_105859) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "reports", force: :cascade do |t|
@@ -85,5 +86,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_09_105859) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "users"
   add_foreign_key "reports", "users"
 end
