bd385a09bacae0c8914f6d2980106a99c6c96895
miyanew <dev38_shin@yahoo.co.jp>
2024-06-27 03:21:47 +0900

    commentとreportにバリデーションを追加した

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,9 @@
 class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   belongs_to :user
+
+  validates :body, presence: true
+  validates :commentable_type, presence: true
+  validates :commentable_id, presence: true
+  validates :user_id, presence: true
 end
