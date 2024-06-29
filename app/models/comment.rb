5a4389a4f5d5c40f2be0c49168664256329c933d
miyanew <dev38_shin@yahoo.co.jp>
2024-06-30 02:19:24 +0900

    不要なバリデーションを削除した

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,7 +5,4 @@ class Comment < ApplicationRecord
   belongs_to :user
 
   validates :body, presence: true
-  validates :commentable_type, presence: true
-  validates :commentable_id, presence: true
-  validates :user_id, presence: true
 end
