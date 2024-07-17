502fc22376c5489ad4103c6170d895b2076bbf01
miyanew <dev38_shin@yahoo.co.jp>
2024-07-17 23:38:44 +0900

    日報作成時に言及レコードを登録する

--- app/models/report.rb
+++ app/models/report.rb
@@ -13,11 +13,33 @@ class Report < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
 
+  after_create :create_report_mentions
+
   def editable?(target_user)
     user == target_user
   end
 
   def created_on
     created_at.to_date
+  end
+
+  private
+
+  def create_report_mentions
+    mentioning_report_ids = extract_mentioning_report_ids
+    mentioning_report_ids.each do | mentioning_id|
+      ReportMention.create(
+        mentioning_report_id: mentioning_id,
+        mentioned_report_id: self.id
+      )
+    end
+  end
+
+  def extract_mentioning_report_ids
+    detected_domain_ports = ["127.0.0.1:3000", "localhost:3000"]
+    regex_patterns = detected_domain_ports.map {|domain_port| /http:\/\/#{domain_port}\/reports\/(\d+)/}
+    extracted_ids = regex_patterns.flat_map {|pattern|content.scan(pattern)}
+    mentioning_ids = extracted_ids.flatten.map(&:to_i).uniq - [id]
+    Report.where(id: mentioning_ids).pluck(:id)
   end
 end
