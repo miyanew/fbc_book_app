a7edab35d56697d09cb2d9678cd55aee9eefc2a8
miyanew <dev38_shin@yahoo.co.jp>
2024-11-24 17:07:53 +0900

    アソシエーションの書き方だけ整理した

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,11 +4,29 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
-  has_many :mentioning_report_mentions, class_name: 'ReportMention', foreign_key: 'mentioning_report_id', dependent: :destroy, inverse_of: :mentioning_report
-  has_many :mentioned_report_mentions, class_name: 'ReportMention', foreign_key: 'mentioned_report_id', dependent: :destroy, inverse_of: :mentioned_report
+  # 自分が他の日報に対して行った言及
+  has_many :mentioning_report_mentions,
+           class_name: 'ReportMention',
+           foreign_key: 'mentioning_report_id',
+           dependent: :destroy,
+           inverse_of: :mentioning_report
 
-  has_many :mentioning_reports, through: :mentioned_report_mentions, source: :mentioning_report
-  has_many :mentioned_reports, through: :mentioning_report_mentions, source: :mentioned_report
+  # 他の日報から受けた言及
+  has_many :mentioned_report_mentions,
+           class_name: 'ReportMention',
+           foreign_key: 'mentioned_report_id',
+           dependent: :destroy,
+           inverse_of: :mentioned_report
+
+  # 自分が言及している日報一覧
+  has_many :mentioning_reports,
+           through: :mentioned_report_mentions,
+           source: :mentioning_report
+
+  # 自分を言及している日報一覧
+  has_many :mentioned_reports,
+           through: :mentioning_report_mentions,
+           source: :mentioned_report
 
   validates :title, presence: true
   validates :content, presence: true
